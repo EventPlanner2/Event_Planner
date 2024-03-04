04be0828574e6db57df2892f57be195783a3d92e
Mohamad-Salha <mohamadsalha88@gmail.com>
2024-03-04 17:37:46 +0200

    some upgrades on MainMenu Class

--- EventPlanner/src/main/java/com/example/MainMenu.java
+++ EventPlanner/src/main/java/com/example/MainMenu.java
@@ -10,57 +10,84 @@ public class MainMenu {
     App app = new App();
     public String username;
     public String password;
+    private User user;
 
 
     Scanner input = new Scanner(System.in);
 
     public void menu() {
         System.out.println(WelcomeString);
+        boolean flag = false;
+        boolean flag2 = false;
         while (true) {
-            System.out.println("Choose 1 or 2 or 3 please : ");
             System.out.println("1. log in");
             System.out.println("2. Sign up");
-            System.out.println("3. Exit");
-            int choose = input.nextInt();
+            System.out.println("X. Exit");
+            System.out.print("Enter your choose please : ");
+            String choose = input.next();
             switch (choose) {
-                case 1: {
-                    logIn();
-                    break;
+                case "1": {
+                    flag = logIn();
+                    if (flag)
+                        break;
                 }
-                case 2: {
-                    SignUp();
-                    break;
+                case "2": {
+                    flag2 = signUp();
+                    if (flag2) {
+                        break;
+                    }
                 }
-                case 3:
-                    System.exit(0);
                 default:
-                    continue;
+                    System.exit(0);
+            }
+            if (flag) {
+                break;
+            }
+            if (flag2) {
+                continue;
             }
         }
     }
 
-    public void logIn() {
-        System.out.println("Please enter the username and the password :");
-        System.out.print("Username :");
-        username = input.next();
-        System.out.println("Password :");
-        password = input.next();
-        User user = app.loginService.LoginPerformed(username, password);
-        char role = user.getRole();
-
+    public boolean logIn() {
+        char role = 'a';
+        while (true) {
+            System.out.print("Please Enter B if you want to go back : ");
+            String back = input.next();
+            if (back.equals("B")) menu();
+            System.out.println("Please enter the username and the password :");
+            System.out.print("Username :");
+            username = input.next();
+            System.out.print("Password :");
+            password = input.next();
+            user = app.loginService.LoginPerformed(username, password);
+            if (user == null) {
+                System.out.println(app.loginService.errorMessage);
+            } else {
+                role = user.getRole();
+                break;
+            }
+        }
         switch (role) {
             case 'a':
+                System.out.println("/n You logged in as Admin");
                 adminPage();
+                break;
 
             case 's':
+                System.out.println("/n You logged in as Serves Provider");
                 serviceProviderPage();
+                break;
 
             case 'c':
+                System.out.println("/n You logged in as Clint");
                 clintPage();
+                break;
         }
+        return true;
     }
 
-    public void SignUp() {
+    public boolean signUp() {
         while (true) {
             System.out.print("Please enter your username :");
             String username = input.next();
@@ -68,28 +95,121 @@ public class MainMenu {
             String password = input.next();
             System.out.print("Please enter your email :");
             String email = input.next();
-            System.out.print("Please enter your role");
+            System.out.print("Please enter your role :");
             String role = input.next();
-            app.signUpService.register(username, password, email, role);
-            if (app.signUpService.username_existed(username)) {
-                System.out.println("This user name is already exists /n Please enter a valid user name");
-            } else if (app.signUpService.email_existed(email)) {
-                System.out.println("This email is already exists /n Please enter a valid email");
+            boolean flag = app.signUpService.register(username, password, email, role);
+            if (flag) {
+                break;
             } else {
+                System.out.println(app.signUpService.msg);
+            }
+        }
+        return true;
+    }
+
+    private void adminPage() {
+        while (true) {
+            System.out.println("1.Show account information ");
+            System.out.println("2.Add Room");
+            System.out.println("3.Search Serves Provider");
+            System.out.println("4.Log out");
+            System.out.println("<- Back");
+            System.out.println("X.Exit");
+            System.out.print("Enter your choose please or Enter B to go Back : ");
+            String choose = input.next();
+            switch (choose) {
+                case "1":
+                    if (accountInformation(user.getRole()))
+                        break;
+                case "2":
+                    addRoom();
+                    break;
+                case "3":
+                    searchServiceProvider();
+                    break;
+                case "4":
+                    menu();
+                    break;
+                case "X":
+                    System.exit(0);
+                default:
+                    break;
+            }
+            if (choose == "B") {
+                break;
+            }
+        }
+
+    }
+
+    private void serviceProviderPage() {
+        while (true) {
+            System.out.println("1.Show account information ");
+            System.out.println("2.Complete");
+            System.out.println("3.Search Serves Provider");
+            System.out.println("4.Log out");
+            System.out.println("<- Back");
+            System.out.println("X.Exit");
+            System.out.print("Enter your choose please or Enter B to go Back : ");
+            String choose = input.next();
+            switch (choose) {
+                case "1":
+                    if (accountInformation(user.getRole()))
+                        break;
+                case "2":
+                    complete();
+                    break;
+                case "3":
+                    searchServiceProvider();
+                    break;
+                case "4":
+                    menu();
+                    break;
+                case "X":
+                    System.exit(0);
+                default:
+                    break;
+            }
+            if (choose == "B") {
                 break;
             }
         }
     }
 
-    private void adminPage() {
-
-    }
-
-    private void serviceProviderPage() {
-
-    }
-
     private void clintPage() {
+        System.out.println("1.Show account information ");
+        System.out.println("2.Upgrade");
+        System.out.println("3.Search Serves Provider");
+        System.out.println("4.Log out");
+        System.out.println("<- Back");
+        System.out.println("X.Exit");
+    }
+
+
+    public boolean accountInformation(char role) {
+        System.out.println("Username : " + user.getUsername());
+        System.out.println("Password : " + user.getPassword());
+        System.out.println("Email : " + user.getContactEmail());
+        System.out.println("Role : " + user.getRole());
+        System.out.println("<-Back ");
+        System.out.println("Please enter B to go back");
+        String back = input.next();
+        if (back == "B") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addRoom() {
+
+    }
+
+    public void searchServiceProvider() {
+
+    }
+
+    public void complete() {
 
     }
 }

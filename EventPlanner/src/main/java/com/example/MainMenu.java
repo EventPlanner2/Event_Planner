afd0c066daeebad6451416202d5ed1bfd1525a98
Mohamad-Salha <mohamadsalha88@gmail.com>
2024-03-07 19:45:12 +0200

    MainMenu updates

--- EventPlanner/src/main/java/com/example/MainMenu.java
+++ EventPlanner/src/main/java/com/example/MainMenu.java
@@ -1,12 +1,14 @@
 package com.example;
 
+import com.example.entites.ServiceProvider;
 import com.example.entites.User;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class MainMenu {
 
-    private static String WelcomeString = new String("Welcome To Study Planner");
+    private final String WelcomeString = "Welcome To Study Planner";
     App app = new App();
     public String username;
     public String password;
@@ -16,45 +18,35 @@ public class MainMenu {
     Scanner input = new Scanner(System.in);
 
     public void menu() {
-        System.out.println(WelcomeString);
-        boolean flag = false;
-        boolean flag2 = false;
         while (true) {
+            System.out.println(WelcomeString);
             System.out.println("1. log in");
             System.out.println("2. Sign up");
             System.out.println("X. Exit");
             System.out.print("Enter your choose please : ");
             String choose = input.next();
             switch (choose) {
-                case "1": {
-                    flag = logIn();
-                    if (flag)
-                        break;
-                }
-                case "2": {
-                    flag2 = signUp();
-                    if (flag2) {
-                        break;
-                    }
-                }
-                default:
+                case "1":
+                    logIn();
+                    break;
+
+                case "2":
+                    signUp();
+                    break;
+
+                case "X":
                     System.exit(0);
-            }
-            if (flag) {
-                break;
-            }
-            if (flag2) {
-                continue;
+                default:
+                    break;
             }
         }
     }
 
-    public boolean logIn() {
+    public void logIn() {
         char role = 'a';
+        boolean flag = false;
         while (true) {
-            System.out.print("Please Enter B if you want to go back : ");
-            String back = input.next();
-            if (back.equals("B")) menu();
+            if (Back()) return;
             System.out.println("Please enter the username and the password :");
             System.out.print("Username :");
             username = input.next();
@@ -68,27 +60,31 @@ public class MainMenu {
                 break;
             }
         }
+
         switch (role) {
             case 'a':
                 System.out.println("/n You logged in as Admin");
-                adminPage();
+                Page("2.Add Room", user.getRole());
                 break;
 
             case 's':
                 System.out.println("/n You logged in as Serves Provider");
-                serviceProviderPage();
+                Page("2.Complete", user.getRole());
                 break;
 
             case 'c':
                 System.out.println("/n You logged in as Clint");
-                clintPage();
+                Page("2.Upgrade", user.getRole());
+                break;
+
+            default:
                 break;
         }
-        return true;
     }
 
-    public boolean signUp() {
+    public void signUp() {
         while (true) {
+            if (Back()) return;
             System.out.print("Please enter your username :");
             String username = input.next();
             System.out.print("Please enter your password :");
@@ -104,100 +100,61 @@ public class MainMenu {
                 System.out.println(app.signUpService.msg);
             }
         }
-        return true;
     }
 
-    private void adminPage() {
+
+    public void Page(String specification, char role) {
         while (true) {
             System.out.println("1.Show account information ");
-            System.out.println("2.Add Room");
+            System.out.println(specification);
             System.out.println("3.Search Serves Provider");
             System.out.println("4.Log out");
-            System.out.println("<- Back");
             System.out.println("X.Exit");
-            System.out.print("Enter your choose please or Enter B to go Back : ");
+            System.out.print("Enter your choose please : ");
+
             String choose = input.next();
             switch (choose) {
                 case "1":
-                    if (accountInformation(user.getRole()))
-                        break;
-                case "2":
-                    addRoom();
+                    accountInformation(role);
+                    break;
+                case "2": {
+                    if (role == 'a') {
+                        addRoom();
+                    } else if (role == 's') {
+                        complete();
+                    } else {
+                        upgrade();
+                    }
+                }
                     break;
                 case "3":
                     searchServiceProvider();
                     break;
                 case "4":
-                    menu();
-                    break;
+                    return;
                 case "X":
                     System.exit(0);
                 default:
                     break;
             }
-            if (choose == "B") {
-                break;
-            }
         }
-
     }
 
-    private void serviceProviderPage() {
+
+    public void accountInformation(char role) {
         while (true) {
-            System.out.println("1.Show account information ");
-            System.out.println("2.Complete");
-            System.out.println("3.Search Serves Provider");
-            System.out.println("4.Log out");
-            System.out.println("<- Back");
-            System.out.println("X.Exit");
-            System.out.print("Enter your choose please or Enter B to go Back : ");
-            String choose = input.next();
-            switch (choose) {
-                case "1":
-                    if (accountInformation(user.getRole()))
-                        break;
-                case "2":
-                    complete();
-                    break;
-                case "3":
-                    searchServiceProvider();
-                    break;
-                case "4":
-                    menu();
-                    break;
-                case "X":
-                    System.exit(0);
-                default:
-                    break;
+            System.out.println("Username : " + user.getUsername());
+            System.out.println("Password : " + user.getPassword());
+            System.out.println("Email : " + user.getContactEmail());
+            if (role == 'a')
+                System.out.println("Role : Admin");
+            else if (role == 's') {
+                System.out.println("Role : Serves Provider");
+            } else {
+                System.out.println("Role : Clint");
             }
-            if (choose == "B") {
-                break;
-            }
-        }
-    }
-
-    private void clintPage() {
-        System.out.println("1.Show account information ");
-        System.out.println("2.Upgrade");
-        System.out.println("3.Search Serves Provider");
-        System.out.println("4.Log out");
-        System.out.println("<- Back");
-        System.out.println("X.Exit");
-    }
-
-
-    public boolean accountInformation(char role) {
-        System.out.println("Username : " + user.getUsername());
-        System.out.println("Password : " + user.getPassword());
-        System.out.println("Email : " + user.getContactEmail());
-        System.out.println("Role : " + user.getRole());
-        System.out.println("<-Back ");
-        System.out.println("Please enter B to go back");
-        String back = input.next();
-        if (back == "B") {
-            return true;
-        } else {
-            return false;
+            if (Back())
+                return;
         }
     }
 
@@ -207,9 +164,83 @@ public class MainMenu {
 
     public void searchServiceProvider() {
 
+        List<ServiceProvider> tmpArray = null;
+        while (true) {
+            if (Back()) return;
+            System.out.println("Search based on : ");
+            System.out.println("1.Location ");
+            System.out.println("2.Type ");
+            System.out.println("3.Price");
+            System.out.print("Please enter your choose :");
+            String choose = input.next();
+            switch (choose) {
+                case "1":
+                    app.SearchSP.setSelectedCriteria("Location");
+                    tmpArray = app.SearchSP.SearchSPPerformed();
+                    printCriteria(tmpArray);
+                    break;
+                case "2":
+                    app.SearchSP.setSelectedCriteria("Type");
+                    tmpArray = app.SearchSP.SearchSPPerformed();
+                    printCriteria(tmpArray);
+                    break;
+                case "3":
+                    app.SearchSP.setSelectedCriteria("Price");
+                    tmpArray = app.SearchSP.SearchSPPerformed();
+                    printCriteria(tmpArray);
+                    break;
+
+                case "":
+                    app.SearchSP.setSelectedCriteria("");
+                    System.out.println(app.SearchSP.getErrorMsg());
+                    break;
+                default:
+                    app.SearchSP.setSelectedCriteria("a");
+                    System.out.println(app.SearchSP.getErrorMsg());
+                    break;
+            }
+        }
     }
 
     public void complete() {
+        String location,productPrice,productType;
+        System.out.println("To complete your account please enter your:");
+        System.out.print("Location : ");
+        location = input.next();
+        System.out.print("Product Price : ");
+        productPrice=input.next();
+        System.out.print("Product Type : ");
+        productType =input.next();
+
+        boolean flag = app.SPAccount.CompleteAccountPerform(location,productPrice,productType);
+
+        if(flag){
+            System.out.println(app.SPAccount.getCompleteAccountMsg());
+            return;
+        }else{
+            System.out.println(app.SPAccount.getCompleteAccountMsg());
+            complete();
+        }
+    }
+
+    public void upgrade() {
 
     }
+
+    public void printCriteria(List<ServiceProvider> ServesProviders) {
+        for (int i = 0; i < ServesProviders.size(); i++) {
+            System.out.println(i + ServesProviders.get(i).getUsername());
+        }
+    }
+
+    public boolean Back() {
+        System.out.print("Please Enter B if you want to go back otherwise enter anything : ");
+        String back = input.next();
+        if (back.equals("B")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

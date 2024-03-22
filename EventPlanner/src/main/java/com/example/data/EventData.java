2b6c4e8813407e98041d669a241b34f9892ac34b
ali-turabi <159777576+ali-turabi@users.noreply.github.com>
2024-03-22 02:16:50 +0200

    DeleteRoom feature steaaaaps and service

--- EventPlanner/src/main/java/com/example/data/EventData.java
+++ EventPlanner/src/main/java/com/example/data/EventData.java
@@ -1,6 +1,5 @@
 package com.example.data;
 
-
 import com.example.entites.Event;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -8,27 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventData {
+    private static final String EVENT_NAME = "Ali Turabi's Event";
+    private static final String CREATOR_NAME = "Ahmad";
+
 
     private static List<Event> events = new ArrayList<>();
-    public static int id_current = 4;
+    private static int ID_CURRENT = 4;
 
-    public EventData() {
+    // Private constructor to prevent direct instantiation
+    private EventData() {
         initializeData();
     }
 
-    private static void initializeData() {
+    private  void initializeData() {
         // Initialize event objects
-        Event e1 = new Event("Ahmad",1, "Ali Turabi's Event", "cars event",
+        Event e1 = new Event(CREATOR_NAME, 1, "Ali Turabi's Event", "cars event",
                 LocalDate.of(2024, 3, 15), LocalDate.of(2024, 4, 15),
                 LocalTime.of(19, 0), LocalTime.of(23, 0), 50);
         e1.setPathImage("image_path1"); // Set image path for event 1
 
-        Event e2 = new Event("Ahmad",2, "Ali Turabi's Event", "mpower bmw version",
+        Event e2 = new Event(CREATOR_NAME, 2, "Ali Turabi's Event", "mpower bmw version",
                 LocalDate.of(2025, 6, 20), LocalDate.of(2025, 6, 20),
                 LocalTime.of(8, 0), LocalTime.of(9, 0), 100);
         e2.setPathImage("image_path2"); // Set image path for event 2
 
-        Event e3 = new Event("Ahmad",3, "Ali Turabi's Event", "mpower bmw version",
+        Event e3 = new Event(CREATOR_NAME, 3, "Ali Turabi's Event", "mpower bmw version",
                 LocalDate.of(2025, 6, 20), LocalDate.of(2025, 6, 20),
                 LocalTime.of(10, 0), LocalTime.of(11, 0), 100);
         e3.setPathImage("image_path3"); // Set image path for event 3
@@ -47,9 +50,7 @@ public class EventData {
         events.add(event);
     }
 
-    public static int generateID(){
-        return id_current++;
+    public static int generateID() {
+        return ID_CURRENT++;
     }
-
-
 }
